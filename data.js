f006d66a4a3fbbc58c4cbf16bf7f55f49960c2cd
karanshah06 <karan.shah@mismo.team>
2024-04-09 13:36:33 -0400

    created a data.js file; imported data into scripts.js for better functionality

---
+++ data.js
@@ -0,0 +1,15 @@
+// This is an array of array of strings (TV show titles and urls at indices 0 and 1 respectively)
+export const titles = [
+  [
+    "Fresh Prince of Bel Air",
+    "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg",
+  ],
+  [
+    "Curb Your Enthusiasm",
+    "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg",
+  ],
+  [
+    "East Los High",
+    "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg",
+  ],
+];
--- scripts.js
+++ scripts.js
@@ -22,22 +22,7 @@
  *    with the string you added to the array, but a broken image.
  *
  */
-
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+import { titles } from "./data.js";
 
 // This function adds cards the page to display the data in the array
 function showCards() {
@@ -46,19 +31,8 @@ function showCards() {
   const templateCard = document.querySelector(".card");
 
   for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
+    let title = titles[i][0];
+    let imageURL = titles[i][1];
     const nextCard = templateCard.cloneNode(true); // Copy the template card
     editCardContent(nextCard, title, imageURL); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
@@ -67,14 +41,11 @@ function showCards() {
 
 function editCardContent(card, newTitle, newImageURL) {
   card.style.display = "block";
-
   const cardHeader = card.querySelector("h2");
   cardHeader.textContent = newTitle;
-
   const cardImage = card.querySelector("img");
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
-
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
   // select "Inspect", then click on the "Console" tab
